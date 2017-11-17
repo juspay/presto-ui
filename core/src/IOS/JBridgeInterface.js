44507d933d8b55a36b3d0a4fea3292ccf84dd9f5
Ronak Jain <ronakjain@outlook.in>
2017-11-18 01:48:14 +0530

    Core: eliminate unused from JbridgeInterface of IOS and WEB polyfills

--- core/src/IOS/JBridgeInterface.js
+++ core/src/IOS/JBridgeInterface.js
@@ -23,26 +23,4 @@
 * along with this program. If not, see <https://www.gnu.org/licenses/agpl.html>.
 */
 
-module.exports = {
-    getSymbol: function() {
-
-    },
-    hideKeyboard: function() {
-
-    },
-    getFromSharedPrefs: function(key) {
-      return localStorage.getItem(key) || "__failed";
-    },
-    setInSharedPrefs: function(key, value) {
-      localStorage.setItem(key, value);
-    },
-    getDeviceDetails: () => {
-      return JSON.stringify({"deviceId":"TEST","packageName":"in.juspay.dui_android","os":"Android","model":"2014818","version":22,"manufacturer":"Xiaomi"});
-    },
-    viewPagerAdapter: function(id, jsx, tabJsx, cb) {
-      return;
-    },
-    setClickFeedback: function() {
-
-    }
-}
+module.exports = {}
