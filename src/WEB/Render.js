14d5100ae458abba7973c48f2bdd342c6b89a331
Ashutosh Das <ashutosh.das@juspay.in>
2019-01-16 14:24:16 +0530

    fix: other events were called with focus event data

--- src/WEB/Render.js
+++ src/WEB/Render.js
@@ -137,8 +137,10 @@ function setAttributes(type, elem, props, firstRender) {
 
         elem['onfocus'] = function (e) {
           elem.parentNode.classList.add('focused');
-          if (eventType == "focus")
-            e.stopPropagation(); cb(e);
+          if (eventType == "focus") {
+            e.stopPropagation();
+            cb(e);
+          }
         };
       }
       if (!(props.label && eventType == "focus")) {
