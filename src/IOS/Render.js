648ebee3ed77e0803fa07d2fac694fa0a0bfefd8
George James <george.james@juspay.in>
2019-09-20 17:19:37 +0530

    remove all UI for IOS

--- src/IOS/Render.js
+++ src/IOS/Render.js
@@ -103,45 +103,51 @@ function inflate(view) {
 }
 
 function runInUI(cmd, fromInflate) {
-  if (!(cmd instanceof Array))
-    cmd = [cmd];
+  if (!(cmd instanceof Array)) cmd = [cmd];
 
-  cmd.forEach(function (each) {
-    const id = each.id;
-    const view = window.__VIEWS[id];
-    if (view){
-      const parent = window.__VIEWS[view.props.parentId];
-      view.props = R.merge(view.props, each);
-      //Adding as stop gag solution for editText in ios where text 
-      //was getting set empty in case other properties were modified.
-      if (!each.hasOwnProperty('text')){
-        delete view.props.text;
-      }
-      // if (each.visibility !== "visible") {
-      //   runInUIHelper(view.type, view.props);
-      // }
-      if (parent && !fromInflate) {
-        if (parent.type.indexOf("scroll") != -1) {
-          inflate(parent);
+  if (cmd.length==1 && cmd[0]=="removeAllUI"){
+    window.webkit.messageHandlers.IOS.postMessage(JSON.stringify({
+      methodName: "removeAllUI",
+      parameters: {"animated":"false"}
+    }));
+  }else{
+    cmd.forEach(function (each) {
+    var id = each.id;
+    var view = window.__VIEWS[id];
+    if (view) {
+        var parent = window.__VIEWS[view.props.parentId];
+        view.props = R.merge(view.props, each);
+        //Adding as stop gag solution for editText in ios where text 
+        //was getting set empty in case other properties were modified.
+        if (!each.hasOwnProperty('text')) {
+          delete view.props.text;
         }
-        computeChildDimens(parent);
-        const children = parent.children;
-        if(!inflate(view)) {
-          runInUIHelper(view.type, view.props, true);
-        };
-        for (let i = 0, len = children.length; i < len; i++) {
-          if (view != children[i]) {
-            inflate(children[i]);
+        // if (each.visibility !== "visible") {
+        //   runInUIHelper(view.type, view.props);
+        // }
+        if (parent && !fromInflate) {
+          if (parent.type.indexOf("scroll") != -1) {
+            inflate(parent);
           }
+          computeChildDimens(parent);
+          var children = parent.children;
+          if (!inflate(view)) {
+            runInUIHelper(view.type, view.props, true);
+          };
+          for (var i = 0, len = children.length; i < len; i++) {
+            if (view != children[i]) {
+              inflate(children[i]);
+            }
+          }
+        } else {
+          runInUIHelper(view.type, view.props);
         }
-      } else {
-        runInUIHelper(view.type, view.props);
+        // if (each.visibility === "visible") {
+        //   runInUIHelper(view.type, view.props);
+        // }
       }
-      // if (each.visibility === "visible") {
-      //   runInUIHelper(view.type, view.props);
-      // }
-    }
-  });
+    });
+  }
 };
 
 function runInUIHelper(type, obj, removeFrame) {
