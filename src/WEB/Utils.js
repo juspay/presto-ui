a8a3434b82b7c4e992970db5a4cbe90d9070c7b0
Siddhant <siddhant.chhabra@juspay.in>
2021-01-14 18:23:35 +0530

    PICAF-3333 Added Missing Util Fn

--- src/WEB/Utils.js
+++ src/WEB/Utils.js
@@ -49,6 +49,15 @@ function convertColorToRgba(color) {
   };
 }
 
+
+function convertHexToRgb(hex) {
+  var r = (parseInt(hex.substring(0, 2), 16)).toFixed(2);
+  var g = (parseInt(hex.substring(2, 4), 16)).toFixed(2);
+  var b = (parseInt(hex.substring(4, 6), 16)).toFixed(2);
+
+  return r + "," + g + "," + b;
+}
+
 // function modifyTranslation(config){
 //   var x = config.x || "0";
 //   var y = config.y || "0";
