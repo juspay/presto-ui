14c72867aac6319671ffc59c9e4c5a89f2aa6ce6
George James <george.james@juspay.in>
2021-08-15 13:35:05 +0530

    PICAF-6188 :: fix for chrome 65 colour

--- src/WEB/Utils.js
+++ src/WEB/Utils.js
@@ -49,9 +49,9 @@ function convertColorToRgba(color) {
 
 
 function convertHexToRgb(hex) {
-  var r = (parseInt(hex.substring(0, 2), 16)).toFixed(2);
-  var g = (parseInt(hex.substring(2, 4), 16)).toFixed(2);
-  var b = (parseInt(hex.substring(4, 6), 16)).toFixed(2);
+  var r = parseInt(hex.substring(0, 2), 16);
+  var g = parseInt(hex.substring(2, 4), 16);
+  var b = parseInt(hex.substring(4, 6), 16);
 
   return r + "," + g + "," + b;
 }
