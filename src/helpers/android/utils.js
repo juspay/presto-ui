4847446d24f005b76f6e8935541d2c74c6589a15
Roopam Garg <roopam.garg@juspay.in>
2023-07-06 13:08:54 +0530

    PICAF-20717: fix: Fixing undefined color issue

--- src/helpers/android/utils.js
+++ src/helpers/android/utils.js
@@ -5,6 +5,7 @@ function getId(config){
 function addAlphaToColor(color, alpha) {
     if(!color) return color;
     if(color.length >= 8) {
+        var colorValue = color.slice(3);
         var alphaInColor = parseInt("0x"+(color.slice(1,3))) / 100;
         var finalAlpha = (alpha * alphaInColor * 100).toString(16);
         return color[0] + finalAlpha + colorValue
